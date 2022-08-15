b9eab44cfb4ca583016f422bce0e40ae7ffc33e1
codingvaleria <codingvaleria@gmail.com>
2022-08-15 23:31:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+
+let multiply = num1 * num2;
+
+//random
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+//remainder
+let num3 = 29;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+//max
+let max = Math.max(5, 20, 17);
+
+
+
+
+
